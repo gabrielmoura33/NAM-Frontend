ffc2a594196a6fff4c2153f8ea65cafadff88694
Gabriel De Moura <gabrielmoura.music@gmail.com>
2020-09-23 23:49:51 -0300

    Production envs

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://nam-backend.gabrielmoura.website/',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
