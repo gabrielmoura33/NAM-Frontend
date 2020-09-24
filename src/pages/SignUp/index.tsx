9dc51c8d9622e4a8b3c528927bf4ddfac9f60732
Gabriel De Moura <gabrielmoura.music@gmail.com>
2020-09-24 08:58:32 -0300

    Fix: signup page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -59,7 +59,8 @@ const SignUp: React.FC = () => {
         addToast({
           type: 'error',
           title: 'Erro na autenticação',
-          description: 'Ocorreu um erro ao fazer login, cheque as credenciais ',
+          description:
+            'Ocorreu um erro ao fazer o cadastro, cheque suas informações obrigatórias! ',
         });
       }
     },
